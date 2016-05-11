3cd41a33d0a5f8051d5a10280cbe0268c4f1aceb
Jamshid <qq@qq.com>
2016-05-11 10:51:16 -0700

    Adding lock to logger.

--- src/Blockchain/Output.hs
+++ src/Blockchain/Output.hs
@@ -3,10 +3,11 @@ module Blockchain.Output where
 
 import Control.Monad.Logger
 import qualified Data.ByteString.Char8 as BC
+import System.GlobalLock
 import System.Log.FastLogger
 
 printLogMsg::Loc->LogSource->LogLevel->LogStr->IO ()
 --printLogMsg loc logSource level msg = do
 printLogMsg _ _ _ msg = do
-    putStrLn $ BC.unpack $ fromLogStr msg
+  lock $ putStrLn $ BC.unpack $ fromLogStr msg
 
